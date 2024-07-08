f9b60d5565d64c10dfc0250e84bfce11383007d7
Golynsky <agolynsky@transas.com>
2024-07-08 13:08:38 +0300

    Simple multiplier implementation for 8-bit MPUs.

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_8x8mul (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,11 +15,26 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+  
+  wire cs = ~ui_in[0];
+  wire rd = ~ui_in[1];
+  wire wr_n = ui_in[2];
+  wire wr = ~wr_n;
+  wire addr = ui_in[7];
+  
+  reg [7:0] A;
+  reg [7:0] B;
+  
+  wire [15:0] Mul = A*B;
+  
+  always @(posedge wr_n) begin
+    if (cs && addr == 0) A = ui_in;
+    if (cs && addr == 1) B = ui_in;
+  end;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out  = Mul[15:8];
+  assign uio_out = addr ? Mul[15:8] : Mul[7:0];
+  assign uio_oe  = cs && rd;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
